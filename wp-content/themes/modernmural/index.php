4a0bec0b1f1e446f2d0339b23f91382d5c57b3a9
Tara Whiteley <tarawhiteley@gmail.com>
2017-07-12 21:48:45 -0400

    adds blog set up to index.php

--- wp-content/themes/modernmural/index.php
+++ wp-content/themes/modernmural/index.php
@@ -1,14 +1,40 @@
 <?php get_header(); ?>
-
 	<main role="main">
 		<!-- section -->
 		<section>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+			<!-- Page Title -->
 
-			<?php get_template_part('partials/loop'); ?>
+			<header class="header">
+			  <h2 class="page-title">Blog</h2>
+			  <h3 class="page-title_tertiary">Designing Your Walls</h3>
+			</header>
 
-			<?php get_template_part('partials/pagination'); ?>
+			<!-- Content -->
+
+			<?php if (have_posts()) : ?>
+			  <div class="blog-grid">
+			    <?php while (have_posts()) : the_post(); ?>
+
+			      <div class="blog-card">
+			        <a href="#">
+			          <img src="http://via.placeholder.com/700x455" alt="" />
+			          <h3 class="blog-card__category"><?php
+			          $all_categories = get_the_category('');
+			          foreach ($all_categories as $one) { ?>
+			            a href="<?=$one->slug; ?>"><?= $one->name; ?></a>&nbsp;
+			            <?php }
+			          ?></h3>
+			          <h2 class="blog-card__title"><?php the_title(); ?></h2>
+			        </a>
+			      </div>
+
+			    <?php endwhile; ?>
+			  </div>
+			<?php else: ?>
+				echo "Sorry nothing was found." ;
+			<?php endif; ?>
+
 
 		</section>
 		<!-- /section -->
