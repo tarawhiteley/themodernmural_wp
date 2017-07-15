b05407f04f906482e45f68321c9ee44acd74e9dd
Tara Whiteley <tarawhiteley@gmail.com>
2017-07-15 10:30:30 -0400

    adds contact about to main page structure

--- wp-content/themes/modernmural/page-home.php
+++ wp-content/themes/modernmural/page-home.php
@@ -50,4 +50,65 @@ get_header(); ?>
 		</div>
 	</main>
 
+	<div class="border"> <!--this is needed -->
+		<h1 class="sectionHeader">The Modern Mural <img src="wp-content\themes\modernmural\img\favicon.png" id="modern_mural_difference"> Difference</h1>
+
+	  <aside class="aboutAside">
+
+	    <img src="wp-content\themes\modernmural\img\jenniferjeffers.jpg" alt="headshot" title="Jennifer Jeffers" class="aboutImage">
+	      <p>Hi, I'm Jennifer Jeffers, the founder and owner of The Modern Mural and its sister site, <a href="www.thesimplestencil.com" alt="the simple stencil" title="The Simple Stencil">The Simple Stencil</a>. I've always had a love affair with words and encouraging others, so it is a dream come true that since 2004, my "job" is to bring meaningful phrases and words of encouragment into my customers homes and professional spaces. It is with that spirit of bringing beauty into people's lives that I created The Modern Mural.</p>
+	      <p>It also brings me joy that The Modern Mural empowers people to create their own custom artwork on their walls. It's a thrill to see what we can create together!</p>
+				<p>Here at The Modern Mural, we are proud to offer you the personalized customer service that only a small business like ours could provide. You're a valuable part of our business, and we appreciate you.</p>
+	  </aside>
+
+	  <section class="aboutSection">
+	    <img src="wp-content\themes\modernmural\img\ecologo.png" class="aboutImg">
+	    <img src="wp-content\themes\modernmural\img\greenguard.png" class="aboutImg">
+	    <h2><a href="#">Learn more about The Difference <br>in The Modern Mural products.</a></h2>
+	      <p>We use HP Latex Inks which provide not only durability and versatility, but also high-quality, odorless prints that reduce the environmental impact by using water-based inks. This innovation offers a healthier solution from the print shop all the way to your space at work or home.</p>
+	  </section>
+
+	</div>
+
+	<div class="border"> <!--trying to get lower border to work -->
+
+		<h1 class="sectionHeader">Contact Us</h1>
+		
+	  <aside class="contactAside">
+	    <h1>We're here to help!</h1>
+	      <p>By far the easiest way to contact us when we are online is to use the "Live Help" option at the top of our support pages. In most cases we will be able to answer your question on the spot. In the unlikely event we can't, we'll respond by email as soon as we have an answer.</p>
+	  </aside>
+
+	  <section class="contactSection">
+	    <article class="contact">
+	      <img src="wp-content\themes\modernmural\img\livechat.png" class="contactImg">
+	      <h2>Live Chat</h2>
+	    </article>
+
+	    <article class="contact">
+	      <img src="wp-content\themes\modernmural\img\email.png" class="contactImg">
+	      <h2>Email</h2>
+	        <p><a href="mailto:TheModernMural@gmail.com">TheModernMural@gmail.com</a></p>
+	    </article>
+
+	    <article class="contact">
+	      <img src="wp-content\themes\modernmural\img\mail.png" class="contactImg">
+	      <h2>Mail</h2>
+	        <p>TheModernMural.com<br>
+	        Jennifer Jeffers LLC<br>
+	        P.O. Box 358998<br>
+	        Gainesville, FL 32635-8998<br>
+	        United States </p>
+	    </article>
+
+	    <article class="contact">
+	      <img src="wp-content\themes\modernmural\img\phone.png" class="contactImg">
+	        <h2>Phone</h2>
+	          <p>US &amp; Canada: <a href="tel:+13522993538">352-299-3538</a></p>
+	          <p>International: Dial your international access code followed by 1-352-299-3538</p>
+	    </article>
+	  </section>
+
+	</div>
+
 <?php get_footer(); ?>
